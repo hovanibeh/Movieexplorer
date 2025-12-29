acac966d438950cc83b1a6044cb76e7d94c649ba
hovanibeh <127843816+hovanibeh@users.noreply.github.com>
2025-12-29 13:00:54 +0000

    Add files via upload
    made the main page

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MovieExplorer")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MovieExplorer.Pages")]
