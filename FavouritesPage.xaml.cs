550fefd554ee093aaa8004309754b7126bbe124f
hovanibeh <127843816+hovanibeh@users.noreply.github.com>
2026-01-01 11:30:28 +0000

    Add files via upload
    i made a favorites page in the pages folder i made an xaml and cs file

---
+++ FavouritesPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MovieExplorer.Models;
+using System.Collections.ObjectModel;
+
+namespace MovieExplorer.Pages
+{
+    public partial class FavouritesPage : ContentPage
+    {
+        // List of favourite movies to display
+        private ObservableCollection<FavouriteMovie> _favourites = new ObservableCollection<FavouriteMovie>();
+
+        public FavouritesPage()
+        {
+            InitializeComponent();
+
+            // Connect the list to the screen
+            FavouritesCollectionView.ItemsSource = _favourites;
+        }
+
+        // Called when page appears - reload favourites
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await LoadFavouritesAsync();
+        }
+
+        // Load favourites from user profile
+        private async Task LoadFavouritesAsync()
+        {
+            var profile = await App.DataService.LoadProfileAsync();
+
+            // Clear and reload
+            _favourites.Clear();
+            foreach (var fav in profile.Favourites)
+            {
+                _favourites.Add(fav);
+            }
+        }
+    }
+}
