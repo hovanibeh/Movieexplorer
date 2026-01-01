bf02292995d3ac48222eea1a82f70fc4b2765217
hovanibeh <127843816+hovanibeh@users.noreply.github.com>
2026-01-01 22:40:10 +0000

    Add files via upload
    my app was take so long to rum so i had to cut things from my maui and app shell xaml and it finally ran

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,31 @@
-﻿namespace MovieExplorer
+﻿using MovieExplorer.Services;
+using MovieExplorer.Pages;
+
+
+namespace MovieExplorer
 {
     public partial class App : Application
     {
+        // Services available throughout the app
+        public static MovieService MovieService { get; private set; } = null!;
+        public static DataService DataService { get; private set; } = null!;
+
         public App()
         {
             InitializeComponent();
+
+            // Create the services
+            MovieService = new MovieService();
+            DataService = new DataService();
         }
 
+        // This creates the main window - NEW .NET 9 way
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            // Simple: just show WelcomePage in a NavigationPage
+            Page startPage = new NavigationPage(new WelcomePage());
+
+            return new Window(startPage);
         }
     }
 }
--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace MovieExplorer
+﻿using MovieExplorer.Pages;
+
+namespace MovieExplorer
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(MovieDetailPage), typeof(MovieDetailPage));
         }
     }
 }
